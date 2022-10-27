8f11fe23eb9b32ef447a94a339db44fdd5e91f35
NAGAKI Yuki <youjiyongmu4@gmail.com>
2022-10-27 12:27:38 +0900

    Update test

--- test.bash
+++ test.bash
@@ -2,6 +2,8 @@
 # SPDX-FileCopyrightText: 2022 NAGAKI Yuki
 # SPDX-License-Identifier: BSD-3-Clause
 
-seq 5 | ./plus
+out=$(seq 5 | ./plus)    # 15 が出ればテスト成功
 
-# 15 が出ればテスト成功
+[ "${out}" = 15 ]
+
+echo $?    # 0 が出ればテスト成功
