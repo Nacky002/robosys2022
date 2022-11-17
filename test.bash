575b3c7c0145cc7fe8bf770826654e9effefb479
NAGAKI Yuki <youjiyongmu4@gmail.com>
2022-11-17 19:53:41 +0900

    check test

--- test.bash
+++ test.bash
@@ -3,7 +3,7 @@
 # SPDX-License-Identifier: BSD-3-Clause
 
 ng () {
-    echo NG at LINE $1
+    echo -e "\nNG at LINE $1"
     res=1
 }
 
@@ -12,26 +12,27 @@ res=0
 ##### I/O #####
 
 out=$(seq 5 | ./plus)
-[ "$?" = 0 ]      || ng $(LINENO)
+[ "$?" = 0 ]      || ng ${LINENO}
 [ "${out}" = 15 ] || ng ${LINENO}           # out=15 で異常なし
 
 out=$(echo 1 2 3 4 5 | tr ' ' '\n' | ./plus)
-[ "$?" = 0 ]      || ng $(LINENO)
+[ "$?" = 0 ]      || ng ${LINENO}
 [ "${out}" = 15 ] || ng ${LINENO}
 
 ##### STRANGE INPUT #####
 
 out=$(echo hoge | ./plus)
-[ "$?" = 1 ]      || ng $(LINENO)
-[ "${out}" = "" ] || ng $(LINENO)
+[ "$?" = 1 ]      || ng ${LINENO}
+[ "${out}" = "" ] || ng ${LINENO}
 
 out=$(echo | ./plus)
-[ "$?" = 1 ]      || ng $(LINENO)
-[ "${out}" = "" ] || ng $(LINENO)
+[ "$?" = 1 ]      || ng ${LINENO}
+[ "${out}" = "" ] || ng ${LINENO}
 
 out=$(echo 推し | ./plus)
-[ "$?" = 1 ]      || ng $(LINENO)
-[ "${out}" = "" ] || ng $(LINENO)
+[ "$?" = 1 ]      || ng ${LINENO}
+[ "${out}" = "" ] || ng ${LINENO}
 
-[ "$res" = 0 ] && echo -e " \nTEST IS SUCCESS !!"
+[ "$res" = 0 ] && echo -e "\nTEST IS SUCCESS !!"
+
 exit $res                                   # このシェルスクリプトの終了ステータスを返して終了
