95a65955e7635678b0698e95ad13b218c93cf2db
NAGAKI Yuki <youjiyongmu4@gmail.com>
2022-11-17 20:33:02 +0900

    check test

--- test.bash
+++ test.bash
@@ -15,7 +15,7 @@ res=0
 
 out=$(seq 5 | ./plus)
 [ "$?" = 0 ]      || ng ${LINENO}
-[ "${out}" = 19 ] || ng ${LINENO}    # out=15 で異常なし
+[ "${out}" = 15 ] || ng ${LINENO}    # out=15 で異常なし
 
 out=$(echo 1 2 3 4 5 | tr ' ' '\n' | ./plus)
 [ "$?" = 0 ]      || ng ${LINENO}
@@ -28,7 +28,7 @@ out=$(echo 推し | ./plus)
 [ "${out}" = "" ] || ng ${LINENO}
 
 out=$(echo | ./plus)
-[ "$?" = 1 ]      || ng ${LINENO}
+[ "$?" = 0 ]      || ng ${LINENO}
 [ "${out}" = "" ] || ng ${LINENO}
 
 [ "$res" = 0 ] && echo -e "\nTEST IS SUCCESS !!"
