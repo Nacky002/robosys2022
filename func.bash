99a953121ff166ea32955d0e0b7afb8eb8d78ee8
NAGAKI Yuki <youjiyongmu4@gmail.com>
2022-11-21 04:39:50 +0900

    add test

---
+++ func.bash
@@ -0,0 +1,35 @@
+#!/bin/bash
+# SPDX-FileCopyrightText: 2022 NAGAKI Yuki
+# SPDX-License-Identifier: BSD-3-Clause
+
+ng () {
+    echo -e "\nNG at LINE $1    TEST IS FAILED"
+    res=1
+    exit $res
+}
+
+res=0
+
+##### I/O #####
+
+out=$(seq 5 | ./plus)
+[ "$?" = 0 ]      || ng ${LINENO}
+[ "${out}" = 15 ] || ng ${LINENO}    # out=15 で異常なし
+
+out=$(echo 1 2 3 4 5 | tr ' ' '\n' | ./plus)
+[ "$?" = 0 ]      || ng ${LINENO}
+[ "${out}" = 15 ] || ng ${LINENO}
+
+##### STRANGE INPUT #####
+
+out=$(echo 推し | ./plus)
+[ "$?" = 1 ]      || ng ${LINENO}
+[ "${out}" = "" ] || ng ${LINENO}
+
+out=$(echo | ./plus)
+[ "$?" = 1 ]      || ng ${LINENO}
+[ "${out}" = "" ] || ng ${LINENO}
+
+[ "$res" = 0 ] && echo -e "\nTEST IS SUCCESS !!"
+
+exit $res                            # このシェルスクリプトの終了ステータスを返して終了
