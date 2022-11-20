985bbc41dc7f60389be8f2fddbdb89ce00747315
NAGAKI Yuki <youjiyongmu4@gmail.com>
2022-11-21 06:05:03 +0900

    update bash

--- avg.bash
+++ avg.bash
@@ -12,23 +12,23 @@ res=0
 
 ##### I/O #####
 
-out=$(seq 5 | ./plus)
-[ "$?" = 0 ]      || ng ${LINENO}
-[ "${out}" = 15 ] || ng ${LINENO}    # out=15 で異常なし
+out=$(seq 5 | ./avg)
+[ "$?" = 0 ]         || ng ${LINENO}
+[ "${out}" = "3.0" ] || ng ${LINENO}    # out=15 で異常なし
 
-out=$(echo 1 2 3 4 5 | tr ' ' '\n' | ./plus)
-[ "$?" = 0 ]      || ng ${LINENO}
-[ "${out}" = 15 ] || ng ${LINENO}
+out=$(echo 1.30 2 3.40 4.80 5 | tr ' ' '\n' | ./avg)
+[ "$?" = 0 ]         || ng ${LINENO}
+[ "${out}" = "3.3" ] || ng ${LINENO}
 
 ##### STRANGE INPUT #####
 
-out=$(echo 推し | ./plus)
-[ "$?" = 1 ]      || ng ${LINENO}
-[ "${out}" = "" ] || ng ${LINENO}
+out=$(echo 推し | ./avg)
+[ "$?" = 1 ]         || ng ${LINENO}
+[ "${out}" = "" ]    || ng ${LINENO}
 
-out=$(echo | ./plus)
-[ "$?" = 1 ]      || ng ${LINENO}
-[ "${out}" = "" ] || ng ${LINENO}
+out=$(echo | ./avg)
+[ "$?" = 1 ]         || ng ${LINENO}
+[ "${out}" = "" ]    || ng ${LINENO}
 
 [ "$res" = 0 ] && echo -e "\nTEST IS SUCCESS !!"
 
